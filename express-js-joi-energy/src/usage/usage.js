87ac8d2f7e41c840f0f04e37d025ddcdc19ca9a4
pavitpim40_mbp <pavitpim@gmail.com>
2024-04-14 17:25:50 +0700

    refactor:usage

--- express-js-joi-energy/src/usage/usage.js
+++ express-js-joi-energy/src/usage/usage.js
@@ -1,37 +1,35 @@
 const average = (readings) => {
-    return (
-        readings.reduce((prev, next) => prev + next.reading, 0) /
-        readings.length
-    );
+  return readings.reduce((prev, next) => prev + next.reading, 0) / readings.length;
 };
 
 const timeElapsedInHours = (readings) => {
-    readings.sort((a, b) => a.time - b.time);
-    const seconds = readings[readings.length - 1].time - readings[0].time;
-    const hours = Math.floor(seconds / 3600);
-    return hours;
+  readings.sort((a, b) => a.time - b.time);
+  const seconds = readings[readings.length - 1].time - readings[0].time;
+  const hours = Math.floor(seconds / 3600);
+  return hours;
 };
 
 const usage = (readings) => {
-    return average(readings) / timeElapsedInHours(readings);
+  return average(readings) / timeElapsedInHours(readings);
 };
 
 const usageCost = (readings, rate) => {
-    return usage(readings) * rate;
+  return usage(readings) * rate;
 };
 
 const usageForAllPricePlans = (pricePlans, readings) => {
-    return Object.entries(pricePlans).map(([key, value]) => {
-        return {
-            [key]: usageCost(readings, value.rate),
-        };
-    });
+  const totalUsage = usage(readings);
+  return Object.entries(pricePlans).map(([key, value]) => {
+    return {
+      [key]: totalUsage * value.rate,
+    };
+  });
 };
 
 module.exports = {
-    average,
-    timeElapsedInHours,
-    usage,
-    usageCost,
-    usageForAllPricePlans,
+  average,
+  timeElapsedInHours,
+  usage,
+  usageCost,
+  usageForAllPricePlans,
 };
